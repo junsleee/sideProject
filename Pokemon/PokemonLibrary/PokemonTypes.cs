ddc31eeed9ea7b90d988559b70028d1017f44be5
JunSeob Lee <junslee1204@gmail.com>
2024-09-15 13:27:41 -0700

    type effectiveness logic implemented

--- Pokemon/PokemonLibrary/PokemonTypes.cs
+++ Pokemon/PokemonLibrary/PokemonTypes.cs
@@ -6,13 +6,37 @@ using System.Threading.Tasks;
 
 namespace Pokemon.PokemonLibrary
 {
-    public static class PokemonTypes
+    public enum PokemonType
     {
-        public enum Type
+        Fire,
+        Water,
+        Grass,
+        Normal
+    }
+    public static class TypeEffectiveness
+    {
+        public static readonly Dictionary<(PokemonType, PokemonType), double> EffectivenessMatrix = new Dictionary<(PokemonType, PokemonType), double>
         {
-            Fire,
-            Water,
-            Grass
+            {(PokemonType.Fire, PokemonType.Grass), 2.0 },
+            {(PokemonType.Grass, PokemonType.Water), 2.0 },
+            {(PokemonType.Water, PokemonType.Fire), 2.0},
+            {(PokemonType.Fire, PokemonType.Water), 0.5},
+            {(PokemonType.Water, PokemonType.Grass), 0.5},
+            {(PokemonType.Grass, PokemonType.Fire), 0.5},
+            {(PokemonType.Fire, PokemonType.Normal), 1.0},
+            {(PokemonType.Grass, PokemonType.Normal), 1.0},
+            {(PokemonType.Water, PokemonType.Normal), 1.0}
+        };
+
+        public static double GetEffectiveness(PokemonType attacker, PokemonType defender)
+        {
+            if (EffectivenessMatrix.TryGetValue((attacker, defender), out double effectiveness))
+            {
+                return effectiveness;
+            }
+            return 1.0;
         }
     }
+
+
 }
